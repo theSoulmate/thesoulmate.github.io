2367977bbbea7f2448763ffac9d87235e844a772
theSoulmate <glfmooo@gmail.com>
2017-06-22 21:10:57 +0300

    assets

---
+++ assets/template/js/app.js
@@ -0,0 +1,16 @@
+$(document).foundation()
+
+$('#primary-menu').on(
+  'show.zf.dropdownmenu', function() {
+    var dropdown = $(this).find('.is-dropdown-submenu');
+    dropdown.css('display', 'none');
+    dropdown.fadeIn('fast');
+});
+$('#primary-menu').on(
+  'hide.zf.dropdownmenu', function() {
+    var dropdown = $(this).find('.is-dropdown-submenu');
+    dropdown.css('display', 'inherit');
+    dropdown.fadeOut('fast');
+});
+
+
